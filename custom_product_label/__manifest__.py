a81d8a5299f4bd94b0df5b7987d8b75fac4d765f
Franco Dequino <francodequino@MacBookAir.fibertel.com.ar>
2025-05-13 10:10:30 -0300

    Actualización

--- custom_product_label/__manifest__.py
+++ custom_product_label/__manifest__.py
@@ -8,6 +8,7 @@
         'views/paperformat.xml',
         'views/my_dymo_template.xml',  
         'views/report_binding.xml',
+        'hide_default_labels.xml',
     ],
     'installable': True,
     'application': False,
